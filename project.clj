6200b9d4791dfdf60f929bc7dceab51c662ae3e5
Lucas Bradstreet <lucasbradstreet@gmail.com>
2016-03-15 04:38:38 +0800

    Fix coords

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject onyx-viz "0.1.0-SNAPSHOT"
+(defproject org.onyxplatform/onyx-viz "0.1.0-SNAPSHOT"
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
