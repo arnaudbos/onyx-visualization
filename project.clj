c08b0ec613c51a7636e1648875978db9e7232c15
Lucas Bradstreet <lucasbradstreet@gmail.com>
2016-03-16 00:50:42 +0800

    Add url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject org.onyxplatform/onyx-viz "0.1.0-SNAPSHOT"
-  :description "FIXME: write this!"
-  :url "http://example.com/FIXME"
+  :description "Visualization for Onyx"
+  :url "https://github.com/lbradstreet/onyx-viz"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
